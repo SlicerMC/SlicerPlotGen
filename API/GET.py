5c4cb721c51690eeca082af7732e88ec45314f8c
Malcolm <137590820+HydrogenOS1@users.noreply.github.com>
2024-03-25 06:24:46 +1000

    Remove The Old POST API Path

--- API/GET.py
+++ API/GET.py
@@ -22,25 +22,5 @@ def get_player_id_by_xpos():
         return player_id
     return 'Hmm... That\'s an error "getNoXpos', 404
 
-@app.route('/set_slice_owner_by_xpos', methods=['POST'])
-def set_slice_owner_by_xpos():
-    player_id = request.args.get('playerId')
-    xpos = request.args.get('xpos')
-    if not player_id or not xpos:
-        return 'Hmm... That\'s an error "setNoXposPID"', 400
-
-    for existing_xpos, existing_player_id in xpos_map.items():
-        if existing_player_id == player_id:
-            return 'Hmm... You already own a slice!', 403
-
-    if xpos in xpos_map:
-        return 'Hmm... That slice is already claimed!', 403
-
-    xpos_map[xpos] = player_id
-    with open('src/database.json', 'w') as f:
-        json.dump(xpos_map, f, indent=4)
-
-    return 'Slice Claimed!'
-
 if __name__ == '__main__':
     app.run(port=8080)
